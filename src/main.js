305d22de1ea4ab99650ce983ce3782ce913ad7e7
Marcelo014 <mfrv214@gmail.com>
2026-04-29 14:40:01 -0400

    feat: phaser rendering on screen

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log('cats vs vacuums')
+import * as Phaser from 'phaser'
+
+const config = {
+  type: Phaser.AUTO,
+  width: 1280,
+  height: 720,
+  backgroundColor: '#1a1a2e',
+  parent: 'game-container',
+  scene: {
+    create() {
+      this.add.text(640, 360, '🐱 Cats vs Vacuums', {
+        fontSize: '48px',
+        fontFamily: 'monospace',
+        color: '#a5d6a7',
+      }).setOrigin(0.5)
+    }
+  }
+}
+
+new Phaser.Game(config)
